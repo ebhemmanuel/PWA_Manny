ae64b72ef5698b16756c7bd593a68195c9ff8781
ebhemmanuel <ebh.emmanuel@gmail.com>
2013-10-02 17:11:14 -0400

    Added comments

--- BarretoEmmanuel/Wk 1/js/main.js
+++ BarretoEmmanuel/Wk 1/js/main.js
@@ -62,13 +62,15 @@
         for (var i = 0; i < 10; i++)
         {
             //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = player1Damage * .5;
-            var minDamage2 = player2Damage * .5;
+            var minDamage1 = player1Damage * .5;       //Pushes out 20 * .5
+            var minDamage2 = player2Damage * .5;       //Pushes out 20 * .5
 
 
 
             var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
             var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //                        f1 and f2 =  Floor it!( // 20 - (20 * .5) + (20 * .5) )
 
             //inflict damage
             playerOneHealth-=f1;
